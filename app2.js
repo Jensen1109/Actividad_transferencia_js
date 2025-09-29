508560bfa8731e4eb784319de7476e629fcbecb3
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:20:09 -0500

    se realizo el segundo ejercicio

---
+++ app2.js
@@ -0,0 +1,6 @@
+let horas = prompt("Ingrese las horas trabajadas")
+let valorhoras = prompt("Ingrese el valor ppr hora")
+
+let pago = horas* valorhoras
+
+alert("El salario semanal del empleado es " +pago)
