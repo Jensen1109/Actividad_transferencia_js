13fa065bf0e729a09705a10c87a88bd381e4c27e
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:34:16 -0500

    se realizo el punto 4

---
+++ app4.js
@@ -0,0 +1,6 @@
+let kilometros = prompt("Ingresa los kilometros recorridos")
+let consumo = prompt("Ingrese el consumo")
+
+let resultado = (Number (kilometros)* Number (consumo))
+
+alert("Litros totales consumidos " +resultado)
