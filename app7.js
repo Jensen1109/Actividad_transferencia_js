a69a14830214ad04dee6ce145a0f795a045b1e31
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:51:17 -0500

    se realizo el punto 7

---
+++ app7.js
@@ -0,0 +1,6 @@
+let precio = prompt("Ingrese el precio del producto")
+
+let prefinal = precio*0.10
+let preciototal = precio-prefinal
+
+alert("El precio final del producto con el descuento del 10% es de " +prefinal)
