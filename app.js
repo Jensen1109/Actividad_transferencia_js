f76f8f9ceff1b1160147d0dd08d15c1f9b25de8e
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:14:33 -0500

    se realizo el primer ejercicio

---
+++ app.js
@@ -0,0 +1,7 @@
+let producto = prompt("Ingrese el nombre del producto")
+let precio = prompt("Ingrese el precio del producto")
+let cantidad = prompt("Ingrese la cantidad comprada")
+
+let total = cantidad*precio
+
+alert("El total a pagar por " + cantidad + producto + " es " +total)
