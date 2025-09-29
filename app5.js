9941e6f09f28b4c01ce1270007b690587ed1bb3c
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:38:51 -0500

    se realizo el punto 5

---
+++ app5.js
@@ -0,0 +1,6 @@
+let distancia = prompt("Ingrese la distancia")
+
+let velocidad = prompt("Ingrese la velocidad ")
+
+let resultado = (Number (distancia) / Number(velocidad))
+alert("El tiempo estimado en horas es " +resultado)
