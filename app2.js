1f84051ab61df9cf79226f6b75e80d1514b168a5
Jensen1109 <rodriguezjensen11@gmail.com>
2025-09-29 17:30:17 -0500

    se realizo el punto 3

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 let horas = prompt("Ingrese las horas trabajadas")
-let valorhoras = prompt("Ingrese el valor ppr hora")
+let valorhoras = prompt("Ingrese el valor por hora")
 
-let pago = horas* valorhoras
+let pago = (horas*valorhoras)*7
 
 alert("El salario semanal del empleado es " +pago)
---
+++ app3.js
@@ -0,0 +1,7 @@
+let edad1 = prompt("Ingrese la primer edad")
+let edad2 = prompt("Ingrese la segunda edad")
+let edad3 = prompt("Ingrese la tercer edad")
+let edad4 = prompt("Ingrese la cuarta edad")
+
+let promedio = (Number (edad1) + Number (edad2) +Number (edad3) +Number(edad4))/4
+alert(promedio)
